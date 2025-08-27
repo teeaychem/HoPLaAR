0f2244ddcea875edb1f57e8a86921ee39bb45fba
teeaychem <bsparkes@stanford.edu>
2025-08-27 14:07:59 +0100

    Negation (e?) normal form
    As in the book.
    
    In addition, a few more redudant clones in the nnf method have been removed.

--- src/logic/normal_form.rs
+++ src/logic/normal_form.rs
@@ -18,7 +18,7 @@ impl<A: Atomic> Formula<A> {
                         match op {
                             And => Formula::Or(lhs.nnf_basic(), rhs.nnf_basic()),
                             Or => Formula::And(lhs.nnf_basic(), rhs.nnf_basic()),
-                            Imp => Formula::And(lhs.nnf_basic(), rhs.clone().negate().nnf_basic()),
+                            Imp => Formula::And(lhs.nnf_basic(), rhs.negate().nnf_basic()),
                             Iff => {
                                 let a = Formula::And(
                                     lhs.clone().nnf_basic(),
@@ -52,12 +52,60 @@ impl<A: Atomic> Formula<A> {
 
             Quantifier { .. } => todo!(),
 
-            _ => self.clone(),
+            _ => self,
         }
     }
 
     pub fn nnf(self) -> Self {
-        self.clone().simplify().nnf_basic()
+        self.simplify().nnf_basic()
+    }
+
+    fn nenf_basic(mut self) -> Self {
+        use {Formula::*, OpBinary::*, OpUnary::*};
+
+        match &mut self {
+            Unary { op, expr } => match op {
+                Not => match expr.as_mut() {
+                    Unary { op, expr } => match op {
+                        Not => std::mem::take(expr).nenf_basic(),
+                    },
+
+                    Binary { op, lhs, rhs } => {
+                        let lhs = std::mem::take(lhs);
+                        let rhs = std::mem::take(rhs);
+
+                        match op {
+                            And => Formula::Or(lhs.nenf_basic(), rhs.nenf_basic()),
+                            Or => Formula::And(lhs.nenf_basic(), rhs.nenf_basic()),
+                            Imp => Formula::And(lhs.nenf_basic(), rhs.negate().nenf_basic()),
+                            Iff => Formula::Iff(lhs.nenf_basic(), rhs.negate().nenf_basic()),
+                        }
+                    }
+
+                    Quantifier { .. } => todo!(),
+
+                    _ => self,
+                },
+            },
+            Binary { op, lhs, rhs } => {
+                let lhs = std::mem::take(lhs);
+                let rhs = std::mem::take(rhs);
+                match op {
+                    And => Formula::And(lhs.nenf_basic(), rhs.nenf_basic()),
+                    Or => Formula::Or(lhs.nenf_basic(), rhs.nenf_basic()),
+                    Imp => Formula::Or(lhs.negate().nenf_basic(), rhs.nenf_basic()),
+                    Iff => Formula::Iff(lhs.nenf_basic(), rhs.nenf_basic()),
+                }
+            }
+
+            Quantifier { .. } => todo!(),
+
+            _ => self,
+        }
+    }
+
+    pub fn nenf(self) -> Self {
+        self.simplify().nenf_basic()
     }
 }
 
@@ -66,10 +114,18 @@ mod tests {
     use crate::logic::{Formula, parse_propositional_formula};
 
     #[test]
-    fn simple() {
+    fn nnf_simple() {
         let expr = parse_propositional_formula("(p <=> q) <=> ~(r ==> s)");
         let expr_nnf = expr.clone().nnf();
 
         assert!(Formula::Iff(expr, expr_nnf).tautology())
+    }
+
+    #[test]
+    fn nenf_simple() {
+        let expr = parse_propositional_formula("(p <=> q) <=> ~(r ==> s)");
+        let expr_nenf = expr.clone().nenf();
+
+        assert!(Formula::Iff(expr, expr_nenf).tautology())
     }
 }
