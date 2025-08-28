327e988aa1af4ff79010a8182a642e12d5d0cb22
teeaychem <bsparkes@stanford.edu>
2025-08-28 17:22:11 +0100

    Improved display for formulas
    Mostly untested, and mostly unthought...

--- src/logic/display.rs
+++ src/logic/display.rs
@@ -1,4 +1,4 @@
-use crate::logic::{propositional::Prop, Formula, Atomic, OpBinary, OpUnary, Quantifier};
+use crate::logic::{Atomic, Formula, OpBinary, OpUnary, Quantifier, propositional::Prop};
 
 impl std::fmt::Display for OpUnary {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -33,9 +33,51 @@ impl<A: Atomic> std::fmt::Display for Formula<A> {
         match self {
             Formula::True => write!(f, "⊤"),
             Formula::False => write!(f, "⊥"),
+
             Formula::Atom { var } => write!(f, "{var}"),
-            Formula::Unary { op, expr } => write!(f, "{op}({expr})"),
-            Formula::Binary { op, lhs, rhs } => write!(f, "({lhs} {op} {rhs})"),
+
+            Formula::Unary { op, expr } => match expr.as_ref() {
+                Formula::True
+                | Formula::False
+                | Formula::Atom { .. }
+                | Formula::Unary { .. }
+                | Formula::Quantifier { .. } => write!(f, "{op}{expr}"),
+
+                Formula::Binary { .. } => write!(f, "{op}({expr})"),
+            },
+
+            Formula::Binary { op, lhs, rhs } => match (lhs.as_ref(), rhs.as_ref()) {
+                (Formula::Binary { op: lop, .. }, Formula::Binary { op: rop, .. }) => {
+                    if op == lop && op == rop {
+                        write!(f, "{lhs} {op} {rhs}")
+                    } else if op == lop {
+                        write!(f, "{lhs} {op} ({rhs})")
+                    } else if op == rop {
+                        write!(f, "({lhs}) {op} {rhs}")
+                    } else {
+                        write!(f, "({lhs}) {op} ({rhs})")
+                    }
+                }
+
+                (Formula::Binary { op: lop, .. }, _) => {
+                    if op == lop {
+                        write!(f, "{lhs} {op} {rhs}")
+                    } else {
+                        write!(f, "({lhs}) {op} {rhs}")
+                    }
+                }
+
+                (_, Formula::Binary { op: rop, .. }) => {
+                    if op == rop {
+                        write!(f, "{lhs} {op} {rhs}")
+                    } else {
+                        write!(f, "{lhs} {op} ({rhs})")
+                    }
+                }
+
+                _ => write!(f, "{lhs} {op} {rhs}"),
+            },
+
             Formula::Quantifier { q, var: atom, expr } => write!(f, "{q}{atom}({expr})"),
         }
     }
