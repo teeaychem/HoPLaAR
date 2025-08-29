95c9780134df26c0ec8cbcb8287a96f990a57330
teeaychem <bsparkes@stanford.edu>
2025-08-29 08:23:54 +0100

    Arithmetic cleanup
    Varied changes to improve ease of reading, and formula generation.
    
    For formula generation, the improvements are from using the fresh conjoin fn, rather than folding.
    
    Elsewhere, ease of reading is improved by shifting things around, a little, rewriting for_each closures as for loops, creating intermediate variables, etc...

--- examples/arithmetic.rs
+++ examples/arithmetic.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use hoplaar::logic::propositional::{Prop, PropFormula, Valuation};
 
 fn bits_needed(i: usize) -> u32 {
@@ -16,57 +18,40 @@ fn bitlength(i: usize) -> u32 {
     if i == 0 { 0 } else { 1 + bitlength(i / 2) }
 }
 
-fn as_bits(i: usize, using: Option<u32>) -> Vec<bool> {
-    let mut remaining = i;
-    let bits_needed = if let Some(required) = using {
-        required
-    } else {
-        bits_needed(i)
+fn as_bits(mut n: usize, bit_count: Option<u32>) -> Vec<bool> {
+    let bits_needed = match bit_count {
+        Some(request) => request,
+        None => bits_needed(n),
     };
-    let base: usize = 2;
-    let mut bit_vec = vec![false; bits_needed.try_into().unwrap()];
-    for x in (0..bits_needed).rev() {
-        if base.pow(x) <= remaining {
-            bit_vec[(bits_needed - (x + 1)) as usize] = true;
-            remaining -= base.pow(x);
+
+    let mut bit_vec = vec![false; bits_needed as usize];
+
+    for idx in (0..bits_needed).rev() {
+        if 2_usize.pow(idx) <= n {
+            bit_vec[(bits_needed - (idx + 1)) as usize] = true;
+            n -= 2_usize.pow(idx);
         }
     }
+
     bit_vec
 }
 
 fn as_int(bits: &[bool]) -> usize {
-    let base: u32 = 2;
     let mut total = 0;
-    for (a, &b) in bits.iter().rev().enumerate() {
-        if b {
-            total += base.pow(a as u32)
+    for (idx, &bit) in bits.iter().rev().enumerate() {
+        if bit {
+            total += 2_usize.pow(idx as u32)
         }
     }
-    total as usize
-}
-
-fn bit(i: usize, n: usize) -> bool {
-    let mut mut_i = i;
-    let mut mut_n = n;
-    loop {
-        if mut_n == 0 {
-            break;
-        } else {
-            mut_n -= 1;
-            mut_i /= 2;
-        }
-    }
-    mut_i % 2 == 1
+    total
 }
 
 fn one_index_atom(id: &str) -> impl Fn(usize) -> PropFormula {
-    let id_str = id.to_string();
-    move |i| PropFormula::Atom(Prop::from(&format!("{id_str}_{i}")))
+    move |i| PropFormula::Atom(Prop::from(&format!("{id}_{i}")))
 }
 
 fn two_index_atom(id: &str) -> impl Fn(usize, usize) -> PropFormula {
-    let id_str = id.to_string();
-    move |i, j| PropFormula::Atom(Prop::from(format!("{}_{}_{}", id_str, i, j).as_str()))
+    move |i, j| PropFormula::Atom(Prop::from(&format!("{id}_{i}_{j}")))
 }
 
 /* Adder */
@@ -110,50 +95,42 @@ fn full_adder(
     )
 }
 
-fn ripple_carry(
-    x: impl Fn(usize) -> PropFormula,
-    y: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    out: impl Fn(usize) -> PropFormula,
-    n: usize,
-) -> PropFormula {
+fn ripple_carry<X, Y, C, O>(x: X, y: Y, c: C, out: O, n: usize) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+    Y: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    O: Fn(usize) -> PropFormula,
+{
     PropFormula::conjoin((0..n).map(|i| full_adder(x(i), y(i), c(i), out(i), c(i + 1))))
 }
 
-fn ripple_carry_0(
-    x: impl Fn(usize) -> PropFormula,
-    y: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    out: impl Fn(usize) -> PropFormula,
-    n: usize,
-) -> PropFormula {
-    ripple_carry(
-        x,
-        y,
-        |i| {
-            if i == 0 { PropFormula::False } else { c(i) }
-        },
-        out,
-        n,
-    )
+fn ripple_carry_0<X, Y, C, O>(x: X, y: Y, c: C, out: O, n: usize) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+    Y: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    O: Fn(usize) -> PropFormula,
+{
+    let c = |i| {
+        if i == 0 { PropFormula::False } else { c(i) }
+    };
+
+    ripple_carry(x, y, c, out, n)
 }
 
-fn ripple_carry_1(
-    x: impl Fn(usize) -> PropFormula,
-    y: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    out: impl Fn(usize) -> PropFormula,
-    n: usize,
-) -> PropFormula {
-    ripple_carry(
-        x,
-        y,
-        |i| {
-            if i == 0 { PropFormula::True } else { c(i) }
-        },
-        out,
-        n,
-    )
+fn ripple_carry_1<X, Y, C, O>(x: X, y: Y, c: C, out: O, n: usize) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+    Y: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    O: Fn(usize) -> PropFormula,
+{
+    let c = |i| {
+        if i == 0 { PropFormula::True } else { c(i) }
+    };
+
+    ripple_carry(x, y, c, out, n)
 }
 
 fn mux(sel: PropFormula, in0: PropFormula, in1: PropFormula) -> PropFormula {
@@ -163,23 +140,29 @@ fn mux(sel: PropFormula, in0: PropFormula, in1: PropFormula) -> PropFormula {
     )
 }
 
-fn offset(n: usize, x: impl Fn(usize) -> PropFormula) -> impl Fn(usize) -> PropFormula {
-    move |i| x(n + i)
-}
-
 #[allow(clippy::too_many_arguments)]
-fn make_fm(
-    x: impl Fn(usize) -> PropFormula,
-    y: impl Fn(usize) -> PropFormula,
-    c0: impl Fn(usize) -> PropFormula,
-    c1: impl Fn(usize) -> PropFormula,
-    s0: impl Fn(usize) -> PropFormula,
-    s1: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    s: impl Fn(usize) -> PropFormula,
+fn make_fm<X, Y, C0, C1, S0, S1, C, S>(
+    x: X,
+    y: Y,
+    c0: C0,
+    c1: C1,
+    s0: S0,
+    s1: S1,
+    c: C,
+    s: S,
     n: usize,
     k: usize,
-) -> PropFormula {
+) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+    Y: Fn(usize) -> PropFormula,
+    C0: Fn(usize) -> PropFormula,
+    C1: Fn(usize) -> PropFormula,
+    S0: Fn(usize) -> PropFormula,
+    S1: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    S: Fn(usize) -> PropFormula,
+{
     let k_n_min = std::cmp::min(n, k);
 
     let f =
@@ -197,27 +180,44 @@ fn make_fm(
     )
 }
 
+fn offset<X>(n: usize, x: X) -> impl Fn(usize) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+{
+    move |i| x(n + i)
+}
+
 #[allow(clippy::too_many_arguments)]
-fn carry_select(
-    x: impl Fn(usize) -> PropFormula,
-    y: impl Fn(usize) -> PropFormula,
-    c0: impl Fn(usize) -> PropFormula,
-    c1: impl Fn(usize) -> PropFormula,
-    s0: impl Fn(usize) -> PropFormula,
-    s1: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    s: impl Fn(usize) -> PropFormula,
+fn carry_select<X, Y, C0, C1, S0, S1, C, S>(
+    x: X,
+    y: Y,
+    c0: C0,
+    c1: C1,
+    s0: S0,
+    s1: S1,
+    c: C,
+    s: S,
     mut n: usize,
     k: usize,
-) -> PropFormula {
+) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+    Y: Fn(usize) -> PropFormula,
+    C0: Fn(usize) -> PropFormula,
+    C1: Fn(usize) -> PropFormula,
+    S0: Fn(usize) -> PropFormula,
+    S1: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    S: Fn(usize) -> PropFormula,
+{
     let mut k_n_min = std::cmp::min(n, k);
+
     let mut fm = make_fm(&x, &y, &c0, &c1, &s0, &s1, &c, &s, n, k);
-    let mut loop_count = 0;
-    loop {
+
+    for loop_count in 0.. {
         if k_n_min < k {
             break;
         } else {
-            loop_count += 1;
             n -= k;
             k_n_min = std::cmp::min(n, k);
             fm = PropFormula::And(
@@ -252,27 +252,23 @@ fn adder_test(n: usize, k: usize) -> PropFormula {
     let c = one_index_atom("c");
     let s = one_index_atom("s");
 
-    PropFormula::Imp(
+    let a = PropFormula::And(
         PropFormula::And(
-            PropFormula::And(
-                carry_select(&x, &y, c0, c1, s0, s1, &c, &s, n, k),
-                c(0).negate(),
-            ),
-            ripple_carry_0(x, y, &c2, &s2, n),
+            carry_select(&x, &y, c0, c1, s0, s1, &c, &s, n, k),
+            c(0).negate(),
         ),
-        PropFormula::And(
-            PropFormula::Iff(c(n), c2(n)),
-            (0..n).fold(PropFormula::True, |acc, i| {
-                PropFormula::And(acc, PropFormula::Iff(s(i), s2(i)))
-            }),
-        ),
-    )
+        ripple_carry_0(x, y, &c2, &s2, n),
+    );
+
+    let c_rhs = PropFormula::conjoin((0..n).map(|i| PropFormula::Iff(s(i), s2(i))));
+    let c = PropFormula::And(PropFormula::Iff(c(n), c2(n)), c_rhs);
+
+    PropFormula::Imp(a, c)
 }
 
 fn ripple_add(x: usize, y: usize) -> usize {
-    let x_size = bits_needed(x);
-    let y_size = bits_needed(y);
-    let max_size = std::cmp::max(x_size, y_size + 1);
+    let max_size = std::cmp::max(bits_needed(x), bits_needed(y) + 1);
+
     let x_bitvec = as_bits(x, Some(max_size));
     let y_bitvec = as_bits(y, Some(max_size));
 
@@ -304,33 +300,30 @@ fn ripple_add(x: usize, y: usize) -> usize {
 
     match the_adder.all_sat_valuations_from(new_valuation).as_slice() {
         [v] => {
-            (0..max_size).for_each(|i| {
+            for i in 0..max_size {
                 out_vec[max_size - (i + 1)] = v.get(&Prop::from(&format!("out_{i}")))
-            });
+            }
 
             as_int(&out_vec)
         }
 
         valuations => {
             println!("Found {} valuations during ripple_add.", valuations.len());
-            for valuation in valuations {
-                println!("{valuation}");
-            }
-
+            // for valuation in valuations {
+            //     println!("{valuation}");
+            // }
             panic!()
         }
     }
 }
 
 fn ripple_add_fm(x: usize, y: usize) -> PropFormula {
-    let x_size = bits_needed(x);
-    let y_size = bits_needed(y);
-    let max_size: usize = std::cmp::max(
-        x_size.try_into().ok().unwrap(),
-        (y_size + 1).try_into().ok().unwrap(),
-    );
-    let x_bitvec = as_bits(x, Some(max_size.try_into().ok().unwrap()));
-    let y_bitvec = as_bits(y, Some(max_size.try_into().ok().unwrap()));
+    let max_size = std::cmp::max(bits_needed(x), bits_needed(y) + 1);
+
+    let x_bitvec = as_bits(x, Some(max_size));
+    let y_bitvec = as_bits(y, Some(max_size));
+
+    let max_size = max_size as usize;
 
     let the_adder = ripple_carry_0(
         one_index_atom("x"),
@@ -340,36 +333,32 @@ fn ripple_add_fm(x: usize, y: usize) -> PropFormula {
         max_size,
     );
 
-    let x_fm = x_bitvec
-        .iter()
-        .rev()
-        .enumerate()
-        .fold(PropFormula::True, |acc, (n_i, n_b)| {
-            let the_atom = PropFormula::Atom(Prop::from(&format!("x_{}", n_i)));
-            let the_negate = the_atom.clone().negate();
-            PropFormula::And(acc, if *n_b { the_atom } else { the_negate })
-        });
+    let x_conjuncts = x_bitvec.iter().rev().enumerate().map(|(n_i, n_b)| {
+        let prop = Prop::from(&format!("x_{n_i}"));
+        let atom = PropFormula::Atom(prop);
+        if *n_b { atom } else { atom.negate() }
+    });
 
-    let y_fm = y_bitvec
-        .iter()
-        .rev()
-        .enumerate()
-        .fold(PropFormula::True, |acc, (n_i, n_b)| {
-            let the_atom = PropFormula::Atom(Prop::from(&format!("y_{}", n_i)));
-            let the_negate = the_atom.clone().negate();
-            PropFormula::And(acc, if *n_b { the_atom } else { the_negate })
-        });
+    let x_fm = PropFormula::conjoin(x_conjuncts);
+
+    let y_conjuncts = y_bitvec.iter().rev().enumerate().map(|(n_i, n_b)| {
+        let prop = Prop::from(&format!("y_{n_i}"));
+        let atom = PropFormula::Atom(prop);
+        if *n_b { atom } else { atom.negate() }
+    });
+
+    let y_fm = PropFormula::conjoin(y_conjuncts);
 
     PropFormula::And(the_adder, PropFormula::And(x_fm, y_fm))
 }
 
 fn carry_add(x: usize, y: usize, k: usize) -> Option<usize> {
-    let x_size = bits_needed(x);
-    let y_size = bits_needed(y);
-    let max_size: usize =
-        std::cmp::max((x_size + 1).try_into().ok()?, (y_size + 1).try_into().ok()?);
-    let x_bitvec = as_bits(x, Some(max_size.try_into().ok()?));
-    let y_bitvec = as_bits(y, Some(max_size.try_into().ok()?));
+    let max_size = std::cmp::max(bits_needed(x), bits_needed(y)) + 1;
+
+    let x_bitvec = as_bits(x, Some(max_size));
+    let y_bitvec = as_bits(y, Some(max_size));
+
+    let max_size = max_size as usize;
 
     let the_carrier = carry_select(
         one_index_atom("x"),
@@ -385,19 +374,20 @@ fn carry_add(x: usize, y: usize, k: usize) -> Option<usize> {
     );
 
     let mut new_valuation = Valuation::from_prop_set(the_carrier.atoms());
-    x_bitvec.iter().rev().enumerate().for_each(|(i, &val)| {
+
+    for (i, &val) in x_bitvec.iter().rev().enumerate() {
         let prop = Prop::from(&format!("x_{i}"));
         new_valuation.set(&prop, val);
         new_valuation.fix(&prop);
-    });
+    }
 
-    y_bitvec.iter().rev().enumerate().for_each(|(i, &val)| {
+    for (i, &val) in y_bitvec.iter().rev().enumerate() {
         let prop = Prop::from(&format!("y_{i}"));
         new_valuation.set(&prop, val);
         new_valuation.fix(&prop);
-    });
+    }
 
-    let mut outvec = vec![false; max_size];
+    let mut out_vec = vec![false; max_size];
     let vals = the_carrier.all_sat_valuations_from(new_valuation);
 
     if vals.len() != 1 {
@@ -405,82 +395,82 @@ fn carry_add(x: usize, y: usize, k: usize) -> Option<usize> {
     }
 
     let v = vals.first()?;
-    (0..max_size)
-        .for_each(|i| outvec[max_size - (i + 1)] = v.get(&Prop::from(&format!("out_{i}"))));
+    for i in 0..max_size {
+        out_vec[max_size - (i + 1)] = v.get(&Prop::from(&format!("out_{i}")))
+    }
 
-    Some(as_int(&outvec))
+    Some(as_int(&out_vec))
 }
 
 /* Multiplier */
 
-#[rustfmt::skip]
-fn ripple_shift(
-    u: impl Fn(usize) -> PropFormula,
-    v: impl Fn(usize) -> PropFormula,
-    c: impl Fn(usize) -> PropFormula,
-    z: PropFormula,
-    w: impl Fn(usize) -> PropFormula,
-    n: usize,
-) -> PropFormula {
-    ripple_carry_0(
-        u,
-        v,
-        |i| { if i == n { w(n - 1) } else { c(i + 1) } },
-        |i| { if i == 0 { z.clone() } else { w(i - 1) } },
-        n,
-    )
+fn ripple_shift<U, V, C, W>(u: U, v: V, c: C, z: PropFormula, w: W, n: usize) -> PropFormula
+where
+    U: Fn(usize) -> PropFormula,
+    V: Fn(usize) -> PropFormula,
+    C: Fn(usize) -> PropFormula,
+    W: Fn(usize) -> PropFormula,
+{
+    let c = |i| {
+        if i == n { w(n - 1) } else { c(i + 1) }
+    };
+
+    let out = |i| {
+        if i == 0 { z.clone() } else { w(i - 1) }
+    };
+
+    ripple_carry_0(u, v, c, out, n)
 }
 
-fn multiplier(
-    x: impl Fn(usize, usize) -> PropFormula,
-    u: impl Fn(usize, usize) -> PropFormula,
-    v: impl Fn(usize, usize) -> PropFormula,
-    out: impl Fn(usize) -> PropFormula,
-    n: usize,
-) -> PropFormula {
-    // println!("n is: {n}");
+fn multiplier<X, U, V, O>(x: X, u: U, v: V, out: O, n: usize) -> PropFormula
+where
+    X: Fn(usize, usize) -> PropFormula,
+    U: Fn(usize, usize) -> PropFormula,
+    V: Fn(usize, usize) -> PropFormula,
+    O: Fn(usize) -> PropFormula,
+{
     if n == 1 {
-        PropFormula::And(PropFormula::Iff(out(0), x(0, 0)), out(1).negate())
-    } else {
-        let the_first_part = ripple_shift(
-            |i| {
-                if i == n - 1 {
-                    PropFormula::False
-                } else {
-                    x(0, i + 1)
-                }
-            },
-            |i| x(1, i),
-            |i| v(2, i),
-            out(1),
-            |i| u(2, i),
-            n,
-        );
-        let the_second_part = if n == 2 {
-            PropFormula::And(
-                PropFormula::Iff(out(2), u(2, 0)),
-                PropFormula::Iff(out(3), u(2, 1)),
-            )
-        } else {
-            (2..n).fold(PropFormula::True, |acc, k| {
-                // println!("fold {k}");
-                let this = ripple_shift(
-                    |i| u(k, i),
-                    |i| x(k, i),
-                    |i| v(k + 1, i),
-                    out(k),
-                    |i| if k == n - 1 { out(n + i) } else { u(k + 1, i) },
-                    n,
-                );
-                PropFormula::And(acc, this)
-            })
-        };
-        PropFormula::And(
-            PropFormula::Iff(out(0), x(0, 0)),
-            PropFormula::And(the_first_part, the_second_part),
-        )
-        .simplify()
+        return PropFormula::And(PropFormula::Iff(out(0), x(0, 0)), out(1).negate());
     }
+
+    let the_first_part = ripple_shift(
+        |i| {
+            if i == n - 1 {
+                PropFormula::False
+            } else {
+                x(0, i + 1)
+            }
+        },
+        |i| x(1, i),
+        |i| v(2, i),
+        out(1),
+        |i| u(2, i),
+        n,
+    );
+
+    if n == 2 {
+        return PropFormula::And(
+            PropFormula::Iff(out(2), u(2, 0)),
+            PropFormula::Iff(out(3), u(2, 1)),
+        );
+    }
+
+    let the_second_part = PropFormula::conjoin((2..n).map(|k| {
+        ripple_shift(
+            |i| u(k, i),
+            |i| x(k, i),
+            |i| v(k + 1, i),
+            out(k),
+            |i| if k == n - 1 { out(n + i) } else { u(k + 1, i) },
+            n,
+        )
+    }));
+
+    PropFormula::And(
+        PropFormula::Iff(out(0), x(0, 0)),
+        PropFormula::And(the_first_part, the_second_part),
+    )
+    .simplify()
 }
 
 fn ripple_multiply(x: usize, y: usize) -> usize {
@@ -500,48 +490,45 @@ fn ripple_multiply(x: usize, y: usize) -> usize {
 
     let mut mul_valuation = Valuation::from_prop_set(the_muler.atoms());
 
-    as_bits(x, Some(max_size))
-        .iter()
-        .rev()
-        .enumerate()
-        .for_each(|(i, &val)| {
-            let prop = Prop::from(&format!("x_{i}"));
-            mul_valuation.set(&prop, val);
-            mul_valuation.fix(&prop);
-        });
+    for (i, &val) in as_bits(x, Some(max_size)).iter().rev().enumerate() {
+        let prop = Prop::from(&format!("x_{i}"));
+        mul_valuation.set(&prop, val);
+        mul_valuation.fix(&prop);
+    }
 
-    as_bits(y, Some(max_size))
-        .iter()
-        .rev()
-        .enumerate()
-        .for_each(|(i, &val)| {
-            let prop = Prop::from(&format!("y_{i}"));
-            mul_valuation.set(&prop, val);
-            mul_valuation.fix(&prop);
-        });
+    for (i, &val) in as_bits(y, Some(max_size)).iter().rev().enumerate() {
+        let prop = Prop::from(&format!("y_{i}"));
+        mul_valuation.set(&prop, val);
+        mul_valuation.fix(&prop);
+    }
 
-    let mut out_vec = vec![false; max_size as usize];
+    let max_size = max_size as usize;
+
+    let mut out_vec = vec![false; max_size];
 
     match the_muler.all_sat_valuations_from(mul_valuation).as_slice() {
         [v] => {
-            (0..max_size).rev().for_each(|i| {
-                out_vec[(max_size - (i + 1)) as usize] = v.get(&Prop::from(&format!("out_{i}")));
-            });
+            for i in 0..max_size {
+                let prop = Prop::from(&format!("out_{i}"));
+                out_vec[max_size - (i + 1)] = v.get(&prop);
+            }
             as_int(&out_vec)
         }
 
         valuations => {
             println!("Found {} valuations during ripple_add.", valuations.len());
-            for valuation in valuations {
-                println!("{valuation}");
-            }
-
+            // for valuation in valuations {
+            //     println!("{valuation}");
+            // }
             panic!()
         }
     }
 }
 
-fn conguruent_to(x: impl Fn(usize) -> PropFormula, m: usize, n: usize) -> PropFormula {
+fn conguruent_to<X>(x: X, m: usize, n: usize) -> PropFormula
+where
+    X: Fn(usize) -> PropFormula,
+{
     PropFormula::conjoin(as_bits(m, Some(n as u32)).iter().rev().enumerate().map(
         |(i, b)| match b {
             true => x(i),
