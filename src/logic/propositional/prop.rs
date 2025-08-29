52a21f2d4bf97fe17f3926ab94d841c2e2567e30
teeaychem <bsparkes@stanford.edu>
2025-08-29 09:05:00 +0100

    An iter for generating sequences of (atomic) propositions

--- src/logic/propositional/prop.rs
+++ src/logic/propositional/prop.rs
@@ -22,3 +22,58 @@ impl Prop {
         self.name = name
     }
 }
+
+struct PropSeq {
+    id: String,
+    idx: usize,
+}
+
+impl PropSeq {
+    fn new(id: &str) -> Self {
+        Self {
+            id: String::from(id),
+            idx: 0,
+        }
+    }
+}
+
+impl Default for PropSeq {
+    fn default() -> Self {
+        Self {
+            id: String::from("p"),
+            idx: 0,
+        }
+    }
+}
+
+impl Iterator for PropSeq {
+    type Item = Prop;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == usize::MAX {
+            None
+        } else {
+            let prop = Prop::from(&format!("{}_{}", self.id, self.idx));
+            self.idx += 1;
+            Some(prop)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::logic::propositional::prop::PropSeq;
+
+    #[test]
+    fn prop_sequences() {
+        let mut props = PropSeq::default();
+        assert_eq!(format!("{}", props.next().unwrap()), "p_0");
+        props.nth(4);
+        assert_eq!(format!("{}", props.next().unwrap()), "p_6");
+
+        let mut props = PropSeq::new("prop");
+        assert_eq!(format!("{}", props.next().unwrap()), "prop_0");
+        props.nth(6);
+        assert_eq!(format!("{}", props.next().unwrap()), "prop_8");
+    }
+}
