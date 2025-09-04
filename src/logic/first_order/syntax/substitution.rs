113870319effa46f2d0b73ad61c0d8bf2f6dc5a0
teeaychem <bsparkes@stanford.edu>
2025-09-04 09:33:58 +0100

    First order syntax module
    With substitution methods moved to a fresh file.

--- src/logic/first_order/syntax/substitution.rs
+++ src/logic/first_order/syntax/substitution.rs
@@ -1,20 +1,11 @@
 use std::collections::HashMap;
 
 use crate::logic::{
-    Formula, Quantifier,
+    Formula,
     first_order::{FirstOrderFormula, Relation, Term, terms::Var},
 };
 
 impl FirstOrderFormula {
-    pub fn generalise(self) -> FirstOrderFormula {
-        let fv = self.free_variables();
-        let mut formula = self;
-        for var in fv {
-            formula = Formula::Quantifier(Quantifier::ForAll, var, formula);
-        }
-        formula
-    }
-
     pub fn term_substitution<S: Fn(Term) -> Term>(self, substitution: &S) -> FirstOrderFormula {
         let mut check = HashMap::default();
         self.term_substitution_local(substitution, &mut check)
@@ -97,18 +88,6 @@ impl FirstOrderFormula {
 mod tests {
 
     use crate::logic::first_order::{Term, parse, terms::Var};
-
-    #[test]
-    fn generalisation() {
-        let expr = parse("R(x,y) => exists z. (R(x,z) & R(z,y))");
-        let generalisation = expr.generalise();
-
-        let q_expr_xy = parse("forall x. forall y. (R(x,y) => exists z. (R(x,z) & R(z,y)))");
-        let q_expr_yx = parse("forall y. forall x. (R(x,y) => exists z. (R(x,z) & R(z,y)))");
-
-        // Ensuring free variables are sorted seems an unreasonable cost.
-        assert!(generalisation == q_expr_xy || generalisation == q_expr_yx)
-    }
 
     #[test]
     fn substitution() {
