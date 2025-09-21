2deb0141fefcd31bf2034989d7252df91a30251d
teeaychem <bsparkes@stanford.edu>
2025-09-21 10:21:38 +0100

    Distributution refinements
    By making use of some mem operations a handful of clones are avoided.

--- src/logic/syntax/mod.rs
+++ src/logic/syntax/mod.rs
@@ -40,15 +40,15 @@ impl<A: Atomic> std::ops::Not for Formula<A> {
     type Output = Formula<A>;
 
     fn not(self) -> Self::Output {
+        use OpUnary::*;
+
         match self {
             Formula::True => Formula::False,
             Formula::False => Formula::True,
 
             Formula::Atom { .. } => Formula::Not(self),
 
-            Formula::Unary { op, expr } => match op {
-                OpUnary::Not => *expr,
-            },
+            Formula::Unary { op: Not, expr } => *expr,
 
             Formula::Binary { .. } => Formula::Not(self),
 
@@ -58,30 +58,36 @@ impl<A: Atomic> std::ops::Not for Formula<A> {
 }
 
 impl<A: Atomic> Formula<A> {
-    pub fn distribute(self) -> Self {
+    pub fn distribute(mut self) -> Self {
         use {Formula::*, OpBinary::*};
 
-        match &self {
+        match &mut self {
             Binary { op: And, lhs, rhs } => {
-                let outer_lhs = &**lhs;
-                let outer_rhs = &**rhs;
+                // Take ownership of the side pointers.
+                // If not distibution happens, these are to be replaced.
+                let mut outer_lhs = std::mem::take(lhs);
+                let mut outer_rhs = std::mem::take(rhs);
 
-                match (outer_lhs, outer_rhs) {
+                match (&mut *outer_lhs, &mut *outer_rhs) {
                     (_, Binary { op: Or, lhs, rhs }) => {
-                        let lhs = Formula::And(outer_lhs.clone(), *lhs.clone());
-                        let rhs = Formula::And(outer_lhs.clone(), *rhs.clone());
+                        let fresh_lhs = Formula::And(*outer_lhs.clone(), std::mem::take(lhs));
+                        let fresh_rhs = Formula::And(*outer_lhs, std::mem::take(rhs));
 
-                        Formula::Or(lhs.distribute(), rhs.distribute())
+                        Formula::Or(fresh_lhs.distribute(), fresh_rhs.distribute())
                     }
 
                     (Binary { op: Or, lhs, rhs }, _) => {
-                        let lhs = Formula::And(*lhs.clone(), outer_rhs.clone());
-                        let rhs = Formula::And(*rhs.clone(), outer_rhs.clone());
+                        let fresh_lhs = Formula::And(std::mem::take(lhs), *outer_rhs.clone());
+                        let fresh_rhs = Formula::And(std::mem::take(rhs), *outer_rhs);
 
-                        Formula::Or(lhs.distribute(), rhs.distribute())
+                        Formula::Or(fresh_lhs.distribute(), fresh_rhs.distribute())
                     }
 
-                    _ => self,
+                    _ => {
+                        *lhs = outer_lhs;
+                        *rhs = outer_rhs;
+                        self
+                    }
                 }
             }
 
@@ -134,7 +140,7 @@ mod tests {
 
         assert!(p.is_literal());
         assert!(p.is_positive_literal());
-        assert!(!p.is_negative_literal());
+        assert!(p.negate().is_negative_literal());
 
         let pq = parse_propositional("p & q");
         assert!(!pq.is_literal());
@@ -157,13 +163,13 @@ mod tests {
 
     #[test]
     fn distribution() {
-        let pqr = parse_propositional("p & (q | r)");
+        let pqr = parse_propositional("p & (q | r)").distribute();
         let expected = parse_propositional("(p & q) | (p & r)");
-        assert_eq!(pqr.distribute(), expected);
+        assert_eq!(pqr, expected);
 
-        let pqr = parse_propositional("(p | q) & r");
+        let pqr = parse_propositional("(p | q) & r").distribute();
         let expected = parse_propositional("(p & r) | (q & r)");
-        assert_eq!(pqr.distribute(), expected);
+        assert_eq!(pqr, expected);
     }
 
     #[test]
